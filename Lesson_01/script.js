17b1ec6c28ace9b0ca629aaadcf18c5cef09bf4c
AleksandraMN <al_mn@mail.ru>
2023-12-28 16:49:45 +1000

    Lesson1

---
+++ Lesson_01/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'Apple';
+let quantity = 10;
+const category = 'Products';
+let priceUnit = 290;
+
+console.log('productName:', productName);
+
+let amount = quantity * priceUnit;
+
+console.log('amount =', amount);
