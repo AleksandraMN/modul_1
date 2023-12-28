e88ae940f6f20ea06da05ae4f6c8428052a650be
AleksandraMN <al_mn@mail.ru>
2023-12-28 17:23:38 +1000

    Lesson2

--- Lesson_01/script.js
+++ Lesson_01/script.js
@@ -1,12 +1,28 @@
 'use strict';
 
-const productName = 'Apple';
+{
+const productName1 = 'Apple';
 let quantity = 10;
 const category = 'Products';
 let priceUnit = 290;
 
-console.log('productName:', productName);
+console.log('productName1:', productName1);
 
-let amount = quantity * priceUnit;
+let amount1 = quantity * priceUnit;
 
-console.log('amount =', amount);
+console.log('amount1 =', amount1);
+}
+
+
+{
+   const productName2 = 'Tangerine';
+   let quantity = 24;
+   const category = 'Products';
+   let priceUnit = 350;
+   
+   console.log('productName2:', productName2);
+   
+   let amount2 = quantity * priceUnit;
+   
+   console.log('amount2 =', amount2);
+   }
